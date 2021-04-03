8d9a09a1406378fbd2b8a8c64840f4a90978b8a7
João Luiz <jleaopitta@gmail.com>
2021-04-03 18:41:03 -0300

    Adicionada responsividade ao Dashboard

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -73,7 +73,12 @@ export default function Dashboard() {
           minChildWidth="320px"
           align="flex-start"
         >
-          <Box p="8" bg="gray.800" borderRadius={8} pb="4">
+          <Box
+            p={["6","8"]}
+            bg="gray.800"
+            borderRadius={8}
+            pb="4"
+          >
             <Text fontSize="lg" mb="4">Inscritos da semana</Text>
             <Chart options={options} series={series} type="area" height={160} />
           </Box>
