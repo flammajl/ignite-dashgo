5218192c4111e01d3cb585b688d2ca61c541a6b5
João Luiz <jleaopitta@gmail.com>
2021-04-03 22:24:21 -0300

    Diminuido espaçamento na criação de usuário

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -11,18 +11,18 @@ export default function UserCreate() {
       <Flex w="100%" my="6" maxW={1480} mx="auto" px="6">
         <Sidebar />
 
-        <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+        <Box flex="1" borderRadius={8} bg="gray.800" p={["6","8"]}>
           <Heading size="lg" fontWeight="normal">Criar usuário</Heading>
 
           <Divider my="6" borderColor="gray.700" />
 
-          <VStack spacing="8">
-            <SimpleGrid minChildWidth="240px" spacing="8" w="100%">
+          <VStack spacing={["6","8"]}>
+            <SimpleGrid minChildWidth="240px" spacing={["6","8"]} w="100%">
               <Input name="name" label="Nome completo" />
               <Input name="email" label="E-mail" type="email" />
             </SimpleGrid>
 
-            <SimpleGrid minChildWidth="240px" spacing="8" w="100%">
+            <SimpleGrid minChildWidth="240px" spacing={["6","8"]} w="100%">
               <Input name="password" label="Senha" type="password" />
               <Input name="password_confirmation" label="Confirmação da senha" type="password" />
             </SimpleGrid>
